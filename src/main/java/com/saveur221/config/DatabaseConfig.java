ff3cc342cbe9154ae1057e37b595cd1c668dc82b
nexuusdev <mamefat2004@gmail.com>
2026-08-30 15:28:22 +0000

    fix: corrections import et interface CategorieRepositoryImpl

--- src/main/java/com/saveur221/config/DatabaseConfig.java
+++ src/main/java/com/saveur221/config/DatabaseConfig.java
@@ -28,7 +28,7 @@ public final class DatabaseConfig {
         //c;asse Utilitaire, non instanciable
     }
 
-    public static Connection getConnexion() throws SQLException{
+    public static Connection getConnetion() throws SQLException{
         return DriverManager.getConnection(
             PROPS.getProperty("db.url"),            
             PROPS.getProperty("db.user"),
