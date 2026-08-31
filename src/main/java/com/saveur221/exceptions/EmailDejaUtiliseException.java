8bf85e3298a1cc51a9999e7ea80260c59a6a29bf
nexuusdev <mamefat2004@gmail.com>
2026-08-31 21:42:19 +0000

    correction et suppression de l'entite client et son interface et repository

--- src/main/java/com/saveur221/exceptions/EmailDejaUtiliseException.java
+++ src/main/java/com/saveur221/exceptions/EmailDejaUtiliseException.java
@@ -1,5 +1,9 @@
-package main.java.com.saveur221.exceptions;
+package com.saveur221.exceptions;
 
-public class EmailDejaUtiliseException {
-    
+public class EmailDejaUtiliseException extends SaveurException {
+
+    public EmailDejaUtiliseException(String message) {
+        super(message);
+    }
+
 }
