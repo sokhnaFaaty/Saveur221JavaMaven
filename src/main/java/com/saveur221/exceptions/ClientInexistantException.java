a1e66d282cc12f9addb4acf7a594708873fa1831
nexuusdev <mamefat2004@gmail.com>
2026-08-31 22:10:45 +0000

    commit

--- src/main/java/com/saveur221/exceptions/ClientInexistantException.java
+++
@@ -1,6 +0,0 @@
-package com.saveur221.exceptions;
-public class ClientInexistantException extends SaveurException {
-    public ClientInexistantException(String message) {
-        super(message);
-    }
-}
